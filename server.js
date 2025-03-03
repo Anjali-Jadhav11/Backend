4989eeb710fdc94c3b38d878be4b8bc86d15d76f
Anjali-Jadhav11 <anjalijadhav20112004@gmail.com>
2025-03-03 11:13:59 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,65 @@
+const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+const User=require('./models/User')
+const bcrypt=require('bcryptjs')
+
+const app=express()
+const PORT=3001
+app.use(express.json());
+
+//home page api
+app.get('/',(req, res)=>{
+    res.send("<h2 align=center>welcome</h2>")
+})
+
+//registration page api
+app.post('/register',async(req,res)=>{
+    const { username, email, password}=req.body
+
+    try{
+        const hashedPassword= await bcrypt.hash(password,10)
+
+        const user=new User({username,email,password:hashedPassword})
+        await user.save()
+        res.join({message: "user register"})
+        console.log("user registration completed")
+
+    }
+    catch(err)
+    {
+        console.log
+    }
+})
+
+//login page api 
+app.post('/login', async(req, res)=>{
+    const {email,password}=req.body
+    try{
+        const user = await User.findOne({ email});
+        if (!user || !(await bcrypt.compare(password, user.password)))
+        {
+            return res.status(400).json({ message: "invalid data"});
+    
+        
+        }
+        res.json({ message: "login Successful", username: user.username});
+
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+mongoose.connect(process.env.MONGO_URL).then(
+    ()=>console.log("db connected successfully")
+).catch(
+    (err)=>console.log(err)
+)
+app.listen(PORT,(err)=>{
+    if(err)
+    {
+        console.log(err)
+    }
+    console.log("server is running on port :"+PORT)
+})
